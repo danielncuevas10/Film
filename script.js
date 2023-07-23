2bb7d8d80bdafa3b8eb7d2a8ea247ce70f0ce2f1
Daniel Navarrete <danielnavarrete2304@gmail.com.com>
2023-07-22 21:52:38 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// Store the image file names in an array
+var images = ['Images/Posters/ThereWill.jpg', 'Images/Posters/Goodfellas.jpg',
+ 'Images/Posters/list.jpg', 'Images/Posters/DarthVader.jpg', 
+ 'Images/Posters/michael.jpg', 
+ 'Images/Posters/Chinatown.jpg','Images/Posters/LOTR.jpg'];
+var currentIndex = 0;
+var imgElement = document.getElementById('myImage');
+
+function changeImage() {
+  // Fade out the image
+  imgElement.style.opacity = 0;
+
+  setTimeout(function() {
+    // Change the image source
+    imgElement.src = images[currentIndex];
+
+    // Fade in the image
+    imgElement.style.opacity = 1;
+
+    // Increment the index
+    currentIndex++;
+
+    // Reset the index to 0 if it exceeds the array length
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+  }, 500); // Adjust the delay to match the CSS transition time
+}
+
+setInterval(changeImage, 4000);
+
+/* JavaScript to handle image expansion */
+
+  function expandImage(img) {
+    var expandedContainer = document.createElement("div");
+    expandedContainer.className = "expanded-image";
+    expandedContainer.onclick = function() {
+      document.body.removeChild(this);
+    };
+
+    var expandedImage = document.createElement("img");
+    expandedImage.src = img.src;
+    expandedImage.alt = img.alt;
+
+    expandedContainer.appendChild(expandedImage);
+    document.body.appendChild(expandedContainer);
+  }
